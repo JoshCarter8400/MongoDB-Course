c56ce146bd7237bc277e077b2844adb7191506e7
Josh Carter <joshcarter8400@gmail.com>
2021-10-28 18:41:00 -0400

    initial setup and connection

---
+++ shell-to-driver-01-starting-code/backend/app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+
+const productRoutes = require('./routes/products');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join('backend/images')));
+
+app.use((req, res, next) => {
+  // Set CORS headers so that the React SPA is able to communicate with this server
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use('/products', productRoutes);
+app.use('/', authRoutes);
+
+const uri = "mongodb+srv://joshCarter:Michael2304@)@cluster0.w3r4c.mongodb.net/mongodb_course?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Connected");
+  client.close();
+});
+
+app.listen(3100);
