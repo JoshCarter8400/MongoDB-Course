ed9247fca5e56fb67fc4a06adf9d373dd025acfb
Josh Carter <joshcarter8400@gmail.com>
2021-10-30 09:27:18 -0400

    added env vars

--- shell-to-driver-01-starting-code/backend/app.js
+++ shell-to-driver-01-starting-code/backend/app.js
@@ -3,30 +3,31 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
+require("dotenv").config();
 
-const productRoutes = require('./routes/products');
-const authRoutes = require('./routes/auth');
+const productRoutes = require("./routes/products");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join('backend/images')));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   // Set CORS headers so that the React SPA is able to communicate with this server
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.use('/products', productRoutes);
-app.use('/', authRoutes);
+app.use("/products", productRoutes);
+app.use("/", authRoutes);
 
-const uri = "mongodb+srv://joshCarter:Michael2304@)@cluster0.w3r4c.mongodb.net/mongodb_course?retryWrites=true&w=majority";
+const uri = `mongodb+srv://joshCarter:${process.env.MONGO_PASS}@cluster0.w3r4c.mongodb.net/mongodb_course?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
